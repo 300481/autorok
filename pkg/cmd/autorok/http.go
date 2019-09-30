199669dc558a9a51b8aed9d29cb34c2c23303a29
300481 <300481@300481.de>
2019-09-30 18:42:31 +0200

    implement install handler

--- pkg/cmd/autorok/http.go
+++ pkg/cmd/autorok/http.go
@@ -1,14 +1,17 @@
 package autorok
 
 import (
+	"io"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func (a *Autorok) routes() {
 	a.Router.HandleFunc("/ipxe", a.ipxeHandler)
 	a.Router.HandleFunc("/boot", a.bootHandler)
-	//a.Router.HandleFunc("/install/{uuid}", a.installHandler)
+	a.Router.HandleFunc("/install/{uuid}", a.installHandler)
 	a.Router.HandleFunc("/rke", a.rkeHandler)
 }
 
@@ -22,12 +25,18 @@ func (a *Autorok) bootHandler(w http.ResponseWriter, r *http.Request) {
 	a.Templates.Boot.Execute(w, a.Config)
 }
 
-/*
 func (a *Autorok) installHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("install.handler:", r.Method, "request from ", r.RemoteAddr)
-	a.Templates.Install.Execute(w, a.Config)
+
+	uuid := mux.Vars(r)["uuid"]
+	n := a.getNode(uuid)
+	if n == nil {
+		log.Println("install.handler: max cluster size reached")
+		io.WriteString(w, "")
+		return
+	}
+	a.Templates.Install.Execute(w, n)
 }
-*/
 
 func (a *Autorok) rkeHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("rke.handler:", r.Method, "request from ", r.RemoteAddr)
